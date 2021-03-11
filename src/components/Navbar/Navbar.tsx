8b27f191fbb737b9d5a7342f8e143c0a964370f5
Albert <gherasim_albert@yahoo.com>
2021-03-11 12:27:54 +0200

    added animation on navbar menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NavDropdown from '../NavDropdown/NavDropdown';
 import './Navbar.scss';
 
 const Navbar: React.FC = () => {
   const [isActive, setActive] = useState(false);
+  const [oldHeight, setOldHeight] = useState(0);
+  const [wasOnceActive, setWasOnceActive] = useState(false);
+  const menuRef = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const menu = menuRef.current;
+    if (menu) {
+      menu.style.display = 'block';
+      setOldHeight(menu.scrollHeight);
+      menu.style.height = '0';
+      menu.style.transition = '.3s height';
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!wasOnceActive) {
+      setWasOnceActive(true);
+    } else {
+      const menu = menuRef.current;
+      if (menu) {
+        if (isActive) {
+          menu.style.height = `${oldHeight}px`;
+          setTimeout(() => {
+            menu.style.height = 'auto';
+          }, 300);
+        } else {
+          menu.style.height = `${menu.scrollHeight}px`;
+          setOldHeight(menu.scrollHeight);
+          menu.style.height = '0';
+        }
+      }
+    }
+  }, [isActive]);
+
   return (
     <nav className="Navbar">
       <div className="Navbar__container container-sm">
@@ -20,7 +54,10 @@ const Navbar: React.FC = () => {
           }}
         ></i>
       </div>
-      <div className={`Navbar__menu${isActive ? ' Navbar__menu--active' : ''}`}>
+      <div
+        className={`Navbar__menu${isActive ? ' Navbar__menu--active' : ''}`}
+        ref={menuRef}
+      >
         <div className="container-sm">
           <div className="Navbar__search_wraper">
             <input
@@ -40,14 +77,14 @@ const Navbar: React.FC = () => {
                 'Trousers & Chinos',
                 'T-Shirts',
               ].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
             </NavDropdown>
             <NavDropdown className="SubNavDropdown" toggler="FORMAL">
               {['Jackets', 'Shirts', 'Suits', 'Trousers'].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
@@ -67,14 +104,14 @@ const Navbar: React.FC = () => {
                 'Trousers & Chinos',
                 'T-Shirts',
               ].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
             </NavDropdown>
             <NavDropdown className="SubNavDropdown" toggler="FORMAL">
               {['Jackets', 'Shirts', 'Suits', 'Trousers'].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
@@ -94,14 +131,14 @@ const Navbar: React.FC = () => {
                 'Men’s Lookbook',
                 'Women’s Lookbook',
               ].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
             </NavDropdown>
             <NavDropdown className="SubNavDropdown" toggler="YOUR LOOKBOOK">
               {['View and Edit', 'Share', 'Delete'].map((item) => (
-                <Link className="NavDropdown__link" to="">
+                <Link className="NavDropdown__link" key={item} to="">
                   {item}
                 </Link>
               ))}
