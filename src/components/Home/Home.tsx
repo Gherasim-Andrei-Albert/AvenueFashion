55fd9b131d37b9aabc117b349d4f33e69b5772ee
Albert <gherasim_albert@yahoo.com>
2021-03-30 10:33:06 +0300

    added products section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderCarousel from '../HeaderCarousel/HeaderCarousel';
+import Products from '../Products/Products';
 import './Home.scss';
 
 const Home: React.FC = () => (
   <div className="Home">
     <HeaderCarousel />
+    <Products />
   </div>
 );
 
